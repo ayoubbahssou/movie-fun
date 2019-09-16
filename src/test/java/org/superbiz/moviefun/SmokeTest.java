7ff4a9ae0dc44fce815457b72c6e1e00b9cdf4f8
ayoubbahssou <ayoub.bahssou@gmail.com>
2019-09-16 15:11:06 +0200

    first and second lab week 2

--- src/test/java/org/superbiz/moviefun/SmokeTest.java
+++ src/test/java/org/superbiz/moviefun/SmokeTest.java
@@ -1,22 +1,37 @@
 package org.superbiz.moviefun;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SmokeTest {
+
+    @LocalServerPort
+    private String port;
+    @Autowired
+    TestRestTemplate restTemplate;
+
 
     @Test
     public void smokeTest() {
-        RestTemplate restTemplate = new RestTemplate();
 
-        String homePage = restTemplate.getForObject(url("/"), String.class);
+        String homePage = restTemplate.getForObject("/", String.class);
 
         assertThat(homePage, containsString("Please select one of the following links:"));
 
-        String setupPage = restTemplate.getForObject(url("/setup.jsp"), String.class);
+        String setupPage = restTemplate.getForObject("/setup", String.class);
 
         assertThat(setupPage, containsString("Wedding Crashers"));
         assertThat(setupPage, containsString("Starsky & Hutch"));
@@ -31,7 +46,7 @@ public class SmokeTest {
     }
 
     private String url(String path) {
-        String baseUrl = "http://localhost:8080/";
+        String baseUrl = "/";
         String envUrl = System.getenv("MOVIE_FUN_URL");
 
         if (envUrl != null && !envUrl.isEmpty()) {

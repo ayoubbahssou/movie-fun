e0687a2449c31dc0466fc04f937652742758d352
ayoubbahssou <mortharia.meftah@gmail.com>
2019-09-18 15:11:55 +0200

    week2 lab 6 logging

--- src/main/java/org/superbiz/moviefun/Application.java
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -3,6 +3,8 @@ package org.superbiz.moviefun;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,6 +13,13 @@ import org.springframework.context.annotation.Bean;
 import org.superbiz.moviefun.blobstore.BlobStore;
 import org.superbiz.moviefun.blobstore.S3Store;
 import org.superbiz.moviefun.blobstore.ServiceCredentials;
+
+import javax.sql.DataSource;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application {
@@ -46,5 +55,19 @@ public class Application {
         }
 
         return new S3Store(s3Client, photoStorageBucket);
+    }
+
+
+
+    @Bean
+    public DatabaseServiceCredentials databaseServiceCredentials(@Value("${vcap.services}") String vcapServices) {
+        return new DatabaseServiceCredentials(vcapServices);
+    }
+
+    @Bean
+    public DataSource albumsDataSource(DatabaseServiceCredentials serviceCredentials) {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(serviceCredentials.jdbcUrl("albums-mysql"));
+        return dataSource;
     }
 }
